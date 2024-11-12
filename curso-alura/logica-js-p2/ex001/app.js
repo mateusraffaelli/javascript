427d3ce7ab88de85c2cfa6dc4ea3b15395945b77
Mateus Raffaelli <mattewscastro@gmail.com>
2024-11-12 00:29:31 -0300

    .

--- curso-alura/logica-js-p2/ex001/app.js
+++ curso-alura/logica-js-p2/ex001/app.js
@@ -3,5 +3,20 @@ titulo.innerHTML = 'Hora do Desafio'
 
 
 function console(){
-    alert('O botão foi clicado')
+    console.log('O botão foi clicado');
+}
+
+function alert(){
+    alert('Eu amo JS');
+}
+
+function prompt(){
+    let cidade = prompt ('Digite o nome de uma cidade do Brasil');
+    alert(`Estive em ${cidade} e lembrei de você`);
+}
+
+function soma(){
+    let numero1 = prompt('Digite um número inteiro')
+    let numero2 = prompt('Digite um número inteiro')
+    alert(numero1 + numero2)
 }
