48596b7c9fbc8db299c4d2f8a39a3cb68061c9a6
Mateus Raffaelli <mattewscastro@gmail.com>
2024-11-28 23:36:45 -0300

    Update app.js

--- curso-alura/logica-js-p2/ex003/app.js
+++ curso-alura/logica-js-p2/ex003/app.js
@@ -2,12 +2,30 @@ function calcularIMC(altura, peso) {
     let IMC = peso / (alturaMetros * alturaMetros)
 }
 
-function calcularFatorial(numero){
-    if(numero < 0){
-        return "O fatorial não é definido para números negativos";
-    }else{
-        if(numero > 1){
-            
-        }
-    }
+function converterDolarParaReal(valorDolar) {
+    let cotacaoDolar = 4.80;
+    let valorReais = valorDolar * cotacaoDolar
+    return valorReais.toFixed(2); // Retorna o valor com duas casas decimais
+}
+
+function calcularAreaPerimetroSalaRetangular(altura, largura) {
+    let area = altura * largura;
+    let perimetro = 2*(altura + largura);
+    console.log(`Área da sala é igual a ${area} metros quadrados`)
+    console.log(`O perímetro da sala é igual a ${perimetro} metros`)
+}
+
+function calcularAreaPerimetroSalaCircular(raio) {
+    let valorPi = 3.14;
+    let area = raio * raio * valorPi
+    let perimetro = 2 * raio * valorPi
+    console.log(`A área da sala mede ${area} metros quadrados`)
+    console.log(`O perímetro da sala mede ${perimetro} metros`)
+}
+
+function mostrarTabuada(numero) {
+    for (let i=1 ; i <= 10; i++){ // i é o número que vai ser multiplicado, ele é = 1 e vai aumentando até chegar no 10
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`);
+  } 
 }
