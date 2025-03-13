c3155a06ff13d6b76cd48d70a9e97a3ed27e21aa
Mateus Raffaelli <mattewscastro@gmail.com>
2025-03-13 19:58:13 -0300

    exercicio 12 js

---
+++ curso-em-video/aula12ex/ex015/script.js
@@ -0,0 +1,13 @@
+let agora = new Date()
+let horario = agora.getHours()
+let divTexto = document.getElementById("texto")
+let divImagem = document.getElementById("imagem")
+
+if(horario >= 6 && horario < 12){
+    a.innerText = 'Agora são '
+}else if(horario >= 12 && horario < 18){
+
+}else if(horario >= 18 && horario <6){
+    a.innerText = 'Agora são oito '
+}
+
