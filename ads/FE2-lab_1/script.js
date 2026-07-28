33f6d218024e4d97007fcaaecf63b7bceb0ed287
Mateus Raffaelli <mattewscastro@gmail.com>
2026-07-28 17:50:30 -0300

    Melhorias em funcionalidades da calculadora

--- ads/FE2-lab_1/script.js
+++ ads/FE2-lab_1/script.js
@@ -2,6 +2,8 @@ const display = document.querySelector('#display')
 
 const teclado = document.querySelector('.keys')
 
+let digitos = [];
+
 teclado.addEventListener('click', (evento) => {
     const botao = evento.target
     if(!botao) return
@@ -15,17 +17,62 @@ teclado.addEventListener('click', (evento) => {
         return
     }
     if(operacao){
-        // registrarOperacao(operacao)
+        registrarOperacao(operacao)
         return
     }
     if(acao){
-        // executarAcao(acao)
+        executarAcao(acao)
         return
     }
 })
 
 const inserirDigito = digito => {
-    display.textContent += digito
+    
+    if(entradaAtual == null || entradaAtual == 0){
+        entradaAtual = digito
+        display.textContent = digito
+    }else{
+        entradaAtual += digito
+        display.textContent += digito
+    }
+    alert(entradaAtual)
+}
+
+const registrarOperacao = operacao => {
+    if(operacao == 'adicao'){
+        operacao = '+'
+    }
+    if(operacao == 'subtracao'){
+        operacao = '-'
+    }
+    if(operacao == 'multiplicacao'){
+        operacao = 'x'
+    }
+    if(operacao == 'divisao'){
+        operacao = '÷'
+    }
+    if(operacao == 'raiz'){
+        operacao = '√'
+    }
+    if(operacao == 'porcento'){
+        operacao = '%'
+    }
+    if(operacao == 'potenciacao'){
+        operacao = ''
+    }
+    
+    valorAnterior = entradaAtual
+    digitos.push(valorAnterior)
+    entradaAtual = null
+
+    display.textContent += operacao
+    alert(entradaAtual)
+    alert(digitos)
+}
+
+const executarAcao = acao => {
+
+    display.textContent = resultado
 }
 
 let entradaAtual = '0'
